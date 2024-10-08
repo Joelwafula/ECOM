312d08495a31c1cfde7cefaef121584d8960870c
Joel <joelwafula2020@gmail.com>
2024-10-08 10:33:11 +0300

    "set contect providers and components"

---
+++ ts-ecom/src/components/Header.tsx
@@ -0,0 +1,22 @@
+
+type PropTypes ={
+    viewCart: boolean,
+    setViewCart: React.Dispatch<React.SetStateAction<boolean>>,
+}
+const Header = ({viewCart,setViewCart}:PropTypes) => {
+
+    const content =(
+        <header className="header">
+            <div className="header__title-bar">
+                
+            </div>
+        </header>
+    )
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Header
