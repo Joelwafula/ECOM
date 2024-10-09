f5baf1280aa9b5970989e3824ba0a71eeffac6a9
Joel <joelwafula2020@gmail.com>
2024-10-09 09:02:11 +0300

    "finished development"

--- ts-ecom/src/components/Header.tsx
+++ ts-ecom/src/components/Header.tsx
@@ -8,7 +8,12 @@ const Header = ({viewCart,setViewCart}:PropTypes) => {
     const content =(
         <header className="header">
             <div className="header__title-bar">
-                
+                <h1>I watch</h1>
+                <div className="header__price-box">
+                    <p>Total items:</p>
+                    <p>Total price:</p>
+                </div>
+
             </div>
         </header>
     )
